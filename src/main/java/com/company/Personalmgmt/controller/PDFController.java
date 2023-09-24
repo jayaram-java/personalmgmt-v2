7ce9f34812d5c4d5a96acc64b0f435b09d108bff
jayaram-java <jayaramp51096@gmail.com>
2023-09-24 23:07:47 +0530

    PDF generate for task

--- src/main/java/com/company/Personalmgmt/controller/PDFController.java
+++ src/main/java/com/company/Personalmgmt/controller/PDFController.java
@@ -12,9 +12,12 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.company.Personalmgmt.dto.EmployeeDto;
 import com.company.Personalmgmt.dto.PersonalInfoDto;
 import com.company.Personalmgmt.model.TimeSheetDetails;
+import com.company.Personalmgmt.pdf.TaskListGenerator;
 import com.company.Personalmgmt.repository.TimeSheetDetailsRepository;
+import com.company.Personalmgmt.service.EmployeeService;
 import com.company.Personalmgmt.service.PersonalInfoService;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
@@ -28,6 +31,9 @@ public class PDFController {
 	
 	@Autowired
 	PersonalInfoService personalInfoService;
+	
+	@Autowired
+	EmployeeService employeeService;
 	
 	@Autowired
 	TimeSheetDetailsRepository timeSheetDetailsRepository;
@@ -132,6 +138,47 @@ public class PDFController {
 		document.add(content3);
 		
 	
+		document.close();
+
+		try {
+			headers.setContentLength(out.size());
+			OutputStream os = response.getOutputStream();
+			out.writeTo(os);
+			os.flush();
+			os.close();
+
+		} catch (Exception e) {
+
+		}
+	}
+	
+	@RequestMapping(value = "/task")
+	public void tasklist(HttpServletResponse response) throws DocumentException, IOException {
+		
+		response.setHeader("Expires", "0");
+		response.setHeader("Cache-Control", "must-revalidate, post-check=0, pre-check=0");
+		response.setHeader("Pragma", "public");
+		response.setContentType("application/pdf");
+
+		HttpHeaders headers = new HttpHeaders();
+
+		Document document = new Document();
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		PdfWriter.getInstance(document, out);
+
+		TaskListGenerator taskListGenerator = new TaskListGenerator();
+		List<EmployeeDto> employeeDtos = employeeService.getAllEmployeeTask();
+
+		document.open();
+		document.addTitle("Task List");
+		document.addSubject("Java");
+		document.addAuthor("Jayaram");
+		
+		PdfPTable header = taskListGenerator.headertwo(employeeDtos);
+
+		Paragraph ob = taskListGenerator.addTitlePage();
+		document.add(ob);
+		document.add(header);
 		document.close();
 
 		try {
